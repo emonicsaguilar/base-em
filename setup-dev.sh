7745cf9e8b8dbd7a80c98284391416827f8e0862
alejandro aguilar higuera <alejandro.aguilar.higuera@gmail.com>
2022-04-09 23:28:12 -0500

    init project

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+sudo apt update -y && sudo apt upgrade -y;
+sudo apt install docker-compose -y;
+
 cat .env.dev > .env;
 cat docker-compose.dev.yml > docker-compose.yml
 cat docker/webapp/Dockerfile.dev.yml > docker/webapp/Dockerfile.yml
 cat docker/nginx/Dockerfile.dev.yml > docker/nginx/Dockerfile.yml
+
+mkdir projects
+cd projects
