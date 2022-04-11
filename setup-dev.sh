2fe0273892da5e1223b267649b096a278863ec98
alejandro aguilar higuera <emonics.aguilar@gmail.com>
2022-04-11 16:52:21 -0500

    add changes setup-dev

--- setup-dev.sh
+++ setup-dev.sh
@@ -3,10 +3,19 @@
 sudo apt update -y && sudo apt upgrade -y;
 sudo apt install docker-compose -y;
 
+
+
+mkdir projects
+cd projects
+git clone https://github.com/emonicsaguilar/api.git
+
+git clone https://github.com/emonicsaguilar/webapp.git
+
+git clone https://github.com/emonicsaguilar/backoffice.git
+
+
+
 cat .env.dev > .env;
 cat docker-compose.dev.yml > docker-compose.yml
 cat docker/webapp/Dockerfile.dev.yml > docker/webapp/Dockerfile.yml
 cat docker/nginx/Dockerfile.dev.yml > docker/nginx/Dockerfile.yml
-
-mkdir projects
-cd projects
