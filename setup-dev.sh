9047e9f06e23276c1e501a5c86524cd6006f6277
alejandro aguilar higuera <emonics.aguilar@gmail.com>
2022-04-11 22:31:26 -0500

    fix setup-dev script

--- setup-dev.sh
+++ setup-dev.sh
@@ -13,9 +13,11 @@ git clone https://github.com/emonicsaguilar/webapp.git
 
 git clone https://github.com/emonicsaguilar/backoffice.git
 
-
+cd ..
 
 cat .env.dev > .env;
+cat projects/api/.env.dev > projects/api/.env
+cat projects/webapp/.env.dev > projects/webapp/.env
 cat docker-compose.dev.yml > docker-compose.yml
 cat docker/webapp/Dockerfile.dev.yml > docker/webapp/Dockerfile.yml
 cat docker/nginx/Dockerfile.dev.yml > docker/nginx/Dockerfile.yml
